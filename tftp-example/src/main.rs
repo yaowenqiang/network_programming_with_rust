a31ca74b48f829752ce64a26f9a7bfbaad921611
yaowenqiang <yaowenqiang111@163.com>
2025-12-06 23:34:46 +0800

    tftp example

---
+++ tftp-example/src/main.rs
@@ -0,0 +1,19 @@
+extern crate tftp_server;
+
+use tftp_server::server::TftpServer;
+use std::net::SocketAddr;
+use std::str::FromStr;
+
+fn main() {
+    let addr = format!("0.0.0.0:{}", 69);
+
+    let socket_addr =  SocketAddr::from_str(addr.as_str()).expect("Error parsing address");
+
+    let mut server = TftpServer::new_from_addr(&socket_addr).expect("Error creating server");
+
+    match server.run() {
+        Ok(_)  => println!("Server completd successfully!"),
+        Err(e) =>  println!("Error: {:?}", e),
+    }
+
+}
