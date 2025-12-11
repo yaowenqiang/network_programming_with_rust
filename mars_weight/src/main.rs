1a1427b5deafa87cb40febbf09d0b42bf26941a2
yaowenqiang <yaowenqiang111@163.com>
2025-12-11 23:00:20 +0800

    add string borrow demo

--- mars_weight/src/main.rs
+++ mars_weight/src/main.rs
@@ -1,7 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Weight on Mars: {} kg", calculate_weight_on_mars(100.0));
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+    println!("Weight {} on earth.",input);
+    
+    let mut mars_weight = calculate_weight_on_mars(100.0);
+    mars_weight  = mars_weight * 1000.0;
+    println!("Weight on Mars: {} kg",mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
+
+// raii
