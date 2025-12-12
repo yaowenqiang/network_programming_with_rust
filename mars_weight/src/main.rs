41b5e0dfa4b52c82ef69040e7e0ddd4712f95b0e
yaowenqiang <yaowenqiang111@163.com>
2025-12-12 21:39:34 +0800

    finish the mars_calc demo

--- mars_weight/src/main.rs
+++ mars_weight/src/main.rs
@@ -1,13 +1,30 @@
 use std::io;
 
 fn main() {
+    println!("Enter your weight (kg):");
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
+    io::stdin().read_line(&mut input).unwrap();
+
+    //borrow_string(&input);
+    //own_string(input);
     println!("Weight {} on earth.",input);
+
+    let weight :f32 = input.trim().parse().unwrap();
+
+    println!("{weight}");
+    dbg!(weight);
     
-    let mut mars_weight = calculate_weight_on_mars(100.0);
+    let mut mars_weight = calculate_weight_on_mars(weight);
     mars_weight  = mars_weight * 1000.0;
     println!("Weight on Mars: {} kg",mars_weight);
+}
+
+fn borrow_string(s: &String) {
+    println!("{}",s);
+}
+
+fn own_string(s: String) {
+    println!("{}", s);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
