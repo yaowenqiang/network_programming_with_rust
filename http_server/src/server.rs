b1ba51a689da4fec89ff693717ab1a560f14d9a0
yaowenqiang <yaowenqiang111@163.com>
2025-12-17 23:00:02 +0800

    add debug

--- http_server/src/server.rs
+++ http_server/src/server.rs
@@ -36,7 +36,9 @@ impl Server {
                        Ok(_) => {
                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
                            match Request::try_from(&buffer[..]) {
-                               Ok(request) => {},
+                               Ok(request) => {
+                                   dbg!(request);
+                               },
                                Err(e) => println!("Failed to parse a request: {}", e),
                            }
                            //let res :&Result<Request, _> = &buffer[..].try_into();
